1834fbd6f90220f91d34f56a8b150627e33df53a
Yu Huieyol <iverlandth@gmail.com>
2016-10-28 12:50:07 -0400

    added field zone for api

--- jobs/api.py
+++ jobs/api.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 class JobSerializer(serializers.ModelSerializer):
     job_id = serializers.SerializerMethodField('get_alternate_name')
     job_type = serializers.SerializerMethodField('get_alternate_type_job')
+    zone = serializers.SerializerMethodField('get_alternate_zone')
 
     class Meta:
         model = Job
@@ -16,3 +17,6 @@ class JobSerializer(serializers.ModelSerializer):
 
     def get_alternate_type_job(self, obj):
         return obj.jobtype.name
+
+    def get_alternate_zone(self, obj):
+        return obj.zone.name
