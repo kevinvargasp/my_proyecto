e9adb13b96c7d17f0b9faf19cc48442460916c86
kevin vargas <kevin.vargas.pastor@gmail.com>
2016-11-05 15:39:52 -0400

    clean images, add option delete partial, add last namen in employees reports

--- notifications/views.py
+++ notifications/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from datetime import datetime
 from notifications.models import Notification
 from users.models import Profile
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import User, Group
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from datetime import datetime
 
+from django.template import RequestContext
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
 
 def notifications_index(request):
     if request.user.is_superuser or Profile.objects.get(user_id=request.user.id).rol == 'SUP':
@@ -23,3 +33,18 @@ def notifications_show(request, id):
         'notification_obj': Notification,
         'notification': notification,
     })
+
+
+def notifications_delete(request, id):
+    notification = Notification.objects.get(id=id)
+    notification.delete()
+    is_exist = Notification.objects.filter(id=id).exists()
+
+    if is_exist:
+        message = 'No se pudo eliminar'
+        messages.add_message(request, messages.ERROR, message)
+    else:
+        message = 'Eliminado!'
+        messages.add_message(request, messages.SUCCESS, message)
+
+    return HttpResponseRedirect(reverse(notifications_index))
--- ssp/local_settings.py
+++ ssp/local_settings.py
@@ -1,12 +1,10 @@
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ssp_dev',
         'USER': 'postgres',
         'PASSWORD': 'root',
         'HOST': '127.0.0.1',
-        'PORT': '5433',
+        'PORT': '5432',
     }
-
 }
-'''
